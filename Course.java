aff7b80b60345dec7d804d1d73cbe540610f95ee
PoisonIvysaurBridge <ivana_lim@dlsu.edu.ph>
2016-08-04 12:23:53 +0800

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,26 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author 11526564
- */
 import java.util.ArrayList;
 
 public class Course 
 {
-    private String name, code;
-    private int units;
+    private String name, 
+    			   code;
+    private int    units;
     private ArrayList<Section> sections;
     
     public Course(String name, String code, int nUnits)
     {
         sections = new ArrayList<>();
         this.name = name;
-        this.code = code;
+        if (code.length() == 7 && code.equals(code.toUpperCase()))
+        	this.code = code;
+        else
+        	System.out.println("invalid course code input.");
         units = nUnits;
     }
     
